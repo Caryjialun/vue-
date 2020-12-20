880ea5e2a81b1c37ccc930e4ff74329c2214dfe0
macbookpro <cary@MacBook-Pro-2.local>
2020-12-20 19:39:55 +0800

    测试

--- src/views/vtooltipDemo.vue
+++ src/views/vtooltipDemo.vue
@@ -4,7 +4,6 @@
         <div class="title-head">
             <img src="../assets/logo.png"/>
             <vtooltip class="t_title" :tooltip="tooltips"></vtooltip>
-            
         </div>
     </div>
 
